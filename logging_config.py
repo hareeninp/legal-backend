a48cd58377ad3b7287edab51f449e27b0526d368
hareeninp <hareeni.np@gmail.com>
2026-08-01 20:53:07 +0530

    Create logging_config.py

---
+++ logging_config.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+
+def get_logger(name: str) -> logging.Logger:
+    return logging.getLogger(name)
